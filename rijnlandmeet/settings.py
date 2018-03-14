92863386d76a861bd2d7f0f7eb0992a70356c065
theo <tkleinen@gmail.com>
2018-03-14 23:35:01 +0100

    adds rijnlandmeet.nl to ALLOWD_HOSTS

--- rijnlandmeet/settings.py
+++ rijnlandmeet/settings.py
@@ -23,7 +23,7 @@ SITE_ID = 1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rijnlandmeet.nl']
 
 INSTALLED_APPS = (
     'grappelli',
